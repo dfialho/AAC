6c89d6a8f9e620af766b02573dae16f118b7e498
davidfialho <fialho.david@gmail.com>
2015-03-16 17:20:09 +0000

    update ao asmToExecutable.py

--- uRisc/asmToExecutable.py
+++ uRisc/asmToExecutable.py
@@ -3,17 +3,21 @@ import os
 import platform # determine the OS 
 from subprocess import call
 
-myAlphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+################ Initializations #################################
 
-def getAppName ():     #plataform test
-    if platform.system() != "Linux":
-        return "OK"
-    else:
-        return "Unsupported"
+myAlphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
 
 currentDir = os.path.dirname(sys.argv[0])
 if currentDir != '':
     currentDir = currentDir + '/'
+
+################ Function Declarations ##########################
+
+def getAppName ():     #plataform test
+    if platform.system() == "Linux":
+        return "OK"
+    else:
+        return "Unsupported"
 
 def check_args():
 
@@ -21,18 +25,24 @@ def check_args():
         print ("(E) Usage: python " + sys.argv[0] + " <file.asm>\n")
         exit()
     
-	completeFileName = currentDir + sys.argv[1]
-	
-	if completeFileName[:-len(".asm")] != ".asm":
-		print ("file provided does not have a '.asm' extension\n")
-		exit()
+    completeFileName = currentDir + sys.argv[1]
+
+    if completeFileName[-len(".asm"):] != ".asm":
+        print ("file provided does not have a '.asm' extension\n")
+        exit()
 	
     return completeFileName
+
+################ SCRIPT BEGIN ####################################
+
+if getAppName() != "OK":
+    print "Script only works on Linux."
+    exit()
 
 completeFileName = check_args()
 filename = completeFileName[:-len(".asm")]
 
-call(["urasm", completeFileName]) 
-call(["urlink", filename + ".obj", "-o " + filename + ".out"])
+call(["urasm", completeFileName], shell=True) 
+call(["urlink", filename + ".obj", "-o", filename + ".out"], shell=True)
 
 print "done!"
