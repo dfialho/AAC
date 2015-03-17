b233eb108dc680ad870fa8c547ae41b93eb03abe
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-03-17 13:03:10 +0000

    script asmToExecutable pronto a usar (dá para melhorar, provavelmente)

--- uRisc/programas/asmToExecutable.py
+++ uRisc/programas/asmToExecutable.py
@@ -7,6 +7,10 @@ from subprocess import call
 
 myAlphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
 
+memfile = "IR.vhd"
+memInitLine = "  constant InitValue : MEM_TYPE := (\n"
+memEndLine  = "  others => X\"0000\");\n"
+
 currentDir = os.path.dirname(sys.argv[0])
 if currentDir != '':
     currentDir = currentDir + '/'
@@ -39,6 +43,88 @@ def check_args():
 	
     return completeFileName, clean
 
+def cleanOutFile(filename):
+    toBeWritten = []
+
+    try:
+        f = open(filename, 'rU')
+    except IOError:
+        print ("(E) Failed to open '" + filename + "'.")
+        exit()
+    
+    lines = f.readlines()
+    i = 0
+    for line in lines:
+        if line.split(" ")[0] == "address":
+            k = int(line.split(" ")[1], 16) - i        
+
+            if i == int(line.split(" ")[1], 16) and i != 0:
+                return toBeWritten
+                #toBeWritten.append('\t' + "others\t=> X\"0000\");")
+
+            while k > 0:
+                toBeWritten.append('\t' + str(i) + "\t=> X\"0000" + "\",\n") # NOP 
+                i += 1
+                k -= 1
+            
+        else:
+            toBeWritten.append('\t' + str(i) + "\t=> X\"" + line.split("\n")[0] + "\",\n")
+            i += 1        
+    
+    f.close()
+    
+    return toBeWritten
+
+def writeToMemory(memfile, toBeWritten):
+    try:
+        f = open(memfile, 'r')
+    except IOError:
+        print ("(E) Failed to open '" + memfile)
+        exit()
+
+    #backup the beginning of the file
+    beginningOfFile = []    
+    line = f.readline()
+    while line != memInitLine:
+        beginningOfFile.append(line)
+
+        line = f.readline()
+        if line == '':
+            print memfile + " does not have the line \"" + memInitLine + "\""
+            f.close
+            exit()
+
+    beginningOfFile.append(line) #append memInitLine
+
+    #backup the end of the file
+    endOfFile = []    
+    line = f.readline()
+    while line != memEndLine:
+        line = f.readline()
+        
+        if line == '':
+            print memfile + " does not have the line \"" + memEndLine + "\""
+            f.close
+            exit()
+
+    #write everythin to the file
+    while line != '':
+        endOfFile.append(line)
+        line = f.readline()
+
+    f.close
+
+    try:
+        f = open(memfile, 'w')
+    except IOError:
+        print ("(E) Failed at writing to " + memfile)
+        exit()
+
+    f.writelines(beginningOfFile)
+    f.writelines(toBeWritten)
+    f. writelines(endOfFile)
+
+    f.close
 
 ################ SCRIPT BEGIN ####################################
 
@@ -54,9 +140,15 @@ if clean == 1:
     print "cleaned up"
     exit()
 
-call(["./urasm " + completeFileName], shell=True) 
-call(["./urlink " + filename + ".obj " + "-o" + filename + ".out"], shell=True)
+#call(["./urasm " + completeFileName], shell=True) 
+#call(["./urlink " + filename + ".obj " + "-o" + filename + ".out"], shell=True)
 
-call(["rm " + filename + ".obj " + filename + ".lis"], shell=True)
+toBeWritten = cleanOutFile(filename + ".out")
+#for line in toBeWritten:
+#    print line
+
+writeToMemory(memfile, toBeWritten)
+
+#call(["rm " + filename + ".obj " + filename + ".lis"], shell=True)
 
 print "done!"
