e27bc969f2870cfc92a2b431e63543cb5e42d38c
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-04-28 00:34:23 +0100

    actualização dos bits de previsão e operação de escrita +- feitas

--- uRisc/BTB_buffer.vhd
+++ uRisc/BTB_buffer.vhd
@@ -5,65 +5,79 @@ use IEEE.std_logic_unsigned.ALL;
 entity BTB_buffer is
 	generic(
 		ADDR_WIDTH : integer := 8;
-		DATA_WIDTH : integer := 8+16+2+1 -- sum of all the contents of the memory: Valid bit, Tag, Target, Pred bits 
+		DATA_WIDTH : integer := 1+8+16+2 -- sum of all the contents of the memory: Valid bit, Tag, Target, Pred bits 
 		);
   port (
-		-- Input
-		clk           : in std_logic;
-		addr_MSB      : in std_logic_vector(7 downto 0);
-		addr_LSB	  : in std_logic_vector(7 downto 0);
-		we            : in std_logic;
-		pc_in         : in std_logic_vector(15 downto 0);
-		dirty_bit_in  : in std_logic;
-		taken		  : in std_logic;
-		prediction_in : in std_logic_vector(1 downto 0);
+  		clk           	: in std_logic;
 
+		-- Read ops
+		readAddr     	: in std_logic_vector(15 downto 0);
+		pcOut         	: out std_logic_vector(15 downto 0);
+		predictionOut 	: out std_logic;
 
-		-- Output
-		tag				: out std_logic_vector(7 downto 0);
-		pc_out         	: out std_logic_vector(15 downto 0);
-		dirty_bit_out  	: out std_logic;
-		prediction_out 	: out std_logic_vector(1 downto 0)
+		-- Write ops
+		writeAddr	  	: in std_logic_vector(15 downto 0);
+		targetAddr		: in std_logic_vector(15 downto 0);
+		taken		  	: in std_logic;
+		we            	: in std_logic
   );
-end entity; -- BTB_buffer
+end entity;
 
 architecture Behavioral of BTB_buffer is
 
 type MEM_TYPE is array (0 to (2**ADDR_WIDTH)-1) of STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
 
 	---- Word
-	-- mem(26) = dirty bit
-	-- mem(25 downto 18) = tag with the less significant bits
-	-- mem(17 downto 2) = target PC
+	-- mem(26) = valid bit
+	-- mem(25 downto 18) = tag: addr MSB
+	-- mem(17 downto 2) = target addr
 	-- mem(1 downto 0) = prediction bits
 	signal mem : MEM_TYPE := (others => "000000000000000000000000000");
 	signal tmp, tmp_out : std_logic_vector(DATA_WIDTH - 1 downto 0);
-	signal newPred : std_logic_vector(1 downto 0);
+	
+	signal tag	: std_logic_vector(7 downto 0);
+	signal oldPredBits, newPredBits : std_logic_vector(1 downto 0);
+	signal prediction : std_logic_vector(1 downto 0);
+	signal lastReadValid, BTBHit	: std_logic;
+
 
 begin
 
-	tmp <= dirty_bit_in & addr_LSB & pc_in & prediction_in;
+---------------------- Writing ----------------------
+	newPredBits(1) <= oldPredBits(0) 		and ((oldPredBits(1) or taken) or (oldPredBits(1) and taken));
+	newPredBits(0) <= (not oldPredBits(0)) 	and ((oldPredBits(1) or taken) or (oldPredBits(1) and taken));
 	
-	newPrediction(1) <= prediction_in(0) 		and (prediction_in(1) or taken) or (prediction_in(1) and taken);
-	newPrediction(0) <= (not prediction_in(0)) 	and (prediction_in(1) or taken) or (prediction_in(1) and taken);
-	---------------------- Writing ----------------------
+	with BTBHit select
+		prediction <= 	newPredBits 		when '1',
+						taken & (not taken)	when '0',
+						"00"	when others;
+
+	tmp <= '1' & writeAddr(7 downto 0) & targetAddr & prediction;
+
 	-- Writing is synchronous
 	Write_Mem : process(clk, we)
 	begin
 		if clk'event and clk = '1' then
 			if we = '1' then
-				mem(conv_integer(addr_MSB)) <= tmp;
+				mem(conv_integer(writeAddr(15 downto 8))) <= tmp;
 			end if ;
 		end if ;
 	end process ; -- Write_Mem
 
-	-------------------- Reading -----------------------
-	-- Reading is asynchronous
-	tmp_out <= mem(conv_integer(addr_MSB));
-	dirty_bit_out <= tmp_out(26);
-	tag <= tmp_out(25 downto 18);
-	pc_out <= tmp_out(17 downto 2);
-	prediction_out <= tmp_out(1 downto 0);
+-------------------- Reading -----------------------
+	oldPrediction : process(clk, we) -- to be used by the write operation
+	begin
+		if clk'event and clk = '1' then
+			oldPredBits <= tmp_out(1 downto 0); -- prediction bits
+			lastReadValid <= tmp_out(26); -- valid bit
+			BTBHit <= '1'; -- TO DO!!!!!! 
+		end if;
+	end process;
 
+	-- Reading is asynchronous
+	tmp_out <= mem(conv_integer(writeAddr(15 downto 8)));
+	tag <= tmp_out(25 downto 18);
+	pcOut <= tmp_out(17 downto 2);
+	predictionOut <= oldPredBits(1);
 
 end Behavioral; -- Behavioral
