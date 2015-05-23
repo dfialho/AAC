c529097f3f8a9c5fbde5ad812ee40c1132acbe25
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-05-23 22:43:35 +0100

    pronto para mostrar ao prof (timings e tal). 2% deerros, erro médio de 0.0027 (perguntar se dá para corrigir)

--- gpu/Smoothing.cu
+++ gpu/Smoothing.cu
@@ -1,23 +1,35 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda.h>
+#include <sys/time.h>
 
 //#define CPUonly
+#define N 50000 // numero de ponto no dataset
 
-void generateData(double vectY[], const double vectX[], size_t vectXSize)
+
+void generateData(float vectY[], const float vectX[])
 {
-    for (size_t i = 0; i < vectXSize; i++)
+    for (size_t i = 0; i < N; i++)
         vectY[i] = sin(0.02 * vectX[i]) + sin(0.001 * vectX[i]) + 0.1 * (rand() / (1.0 * RAND_MAX));
 }
 
-void aproximateValuesCPU(double y[], double x[], double yest[], int smooth, size_t N)
+float timeDiff(struct timespec tStart, struct timespec tEnd)
+{
+   struct timespec diff;
+
+   diff.tv_sec  = tEnd.tv_sec  - tStart.tv_sec  - (tEnd.tv_nsec<tStart.tv_nsec?1:0);
+   diff.tv_nsec = tEnd.tv_nsec - tStart.tv_nsec + (tEnd.tv_nsec<tStart.tv_nsec?1000000000:0);
+
+   return ((float) diff.tv_sec) + ((float) diff.tv_nsec)/1e9;
+}
+
+void aproximateValuesCPU(float y[], float x[], float yest[], int smooth)
 {
     for (size_t i = 0; i < N; i++)
     {
-        double sumA = 0.0;
-        double sumB = 0.0;
-        double temp = 0.0;
+        float sumA = 0.0;
+        float sumB = 0.0;
+        float temp = 0.0;
         for (size_t j = 0; j < N; j++)
         {
             temp = exp((-1 * ((x[i] - x[j]) * (x[i] - x[j]))) / (2 * smooth * smooth));
@@ -28,14 +40,14 @@ void aproximateValuesCPU(double y[], double x[], double yest[], int smooth, size
     }
 }
 
-__global__ void aproximateValuesGPU(double y[], double x[], double yest[], int smooth, size_t N)
+__global__ void aproximateValuesGPU(float y[], float x[], float yest[], int smooth)
 {
     size_t i = threadIdx.x;
     if (i < N)
     {
-        double sumA = 0.0;
-        double sumB = 0.0;
-        double temp = 0.0;
+        float sumA = 0.0;
+        float sumB = 0.0;
+        float temp = 0.0;
         for (size_t j = 0; j < N; j++)
         {
             temp = exp((-1 * ((x[i] - x[j]) * (x[i] - x[j]))) / (2 * smooth * smooth));
@@ -49,65 +61,165 @@ __global__ void aproximateValuesGPU(double y[], double x[], double yest[], int s
 
 int main()
 {
-
-    size_t N = 10000;  // numero de ponto no dataset
     int smooth = 4; // parametro de suavidade
 
+    struct timespec timeVect[7];
+    float timeCPU, timeGPU[7];
+    cudaError_t err[] = { cudaSuccess , cudaSuccess , cudaSuccess };
+
     // criar o dataset de entrada
-    double *x = (double*)malloc(N * sizeof(double));
-    double *y = (double*)malloc(N * sizeof(double));
-    double *yest = (double*)malloc(N * sizeof(double));
+    float *x = (float*)malloc(N * sizeof(float));
+    float *y = (float*)malloc(N * sizeof(float));
+    float *yest = (float*)malloc(N * sizeof(float));
+    float *yestCPU = (float*)malloc(N * sizeof(float));
+
+    // Verify that allocations succeeded
+    if (x == NULL || y == NULL || yest == NULL || yestCPU == NULL )
+    {
+        fprintf(stderr, "Failed to allocate on the host!\n");
+        exit(EXIT_FAILURE);
+    }
 
     for (size_t i = 0; i < N; i++)
         x[i] = i / 10.0;
 
-    generateData(y, x, N);
+    generateData(y, x);
 
-#ifdef CPUonly  
-    aproximateValuesCPU(y, x, yest, smooth, N);
+/* CPU */
+    printf("Performing the computation on the CPU...\n");
+    clock_gettime(CLOCK_REALTIME, &timeVect[0]);
 
-#else
-    double* d_x;
-    double* d_y;
-    double* d_yest; //d_ quer dizer variável do dispositivo, convenção
+    aproximateValuesCPU(y, x, yestCPU, smooth);
 
-    cudaMalloc(&d_x, N*sizeof(double));
-    cudaMalloc(&d_y, N*sizeof(double));
-    cudaMalloc(&d_yest, N*sizeof(double));
+    clock_gettime(CLOCK_REALTIME, &timeVect[1]);
+    timeCPU = timeDiff(timeVect[0],timeVect[1]);
+    printf("cpu done ... execution took %.6f seconds\n", timeCPU);
 
-    cudaMemcpy(d_x, x, N*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, N*sizeof(double), cudaMemcpyHostToDevice);
+    printf("\n----------------------------------------------------------------------------\n\n");
+/* GPU */
 
+    printf("Performing the computation on the GPU...\n");
+    clock_gettime(CLOCK_REALTIME, &timeVect[0]);
+    clock_gettime(CLOCK_REALTIME, &timeVect[1]);
+
+    // initialize the device (just measure the time for the first call to the device)
+    clock_gettime(CLOCK_REALTIME, &timeVect[0]);
+    cudaFree(0);
+    clock_gettime(CLOCK_REALTIME, &timeVect[1]);
+
+    printf(" ... Allocation of memory on the Device ...\n");
+    float* d_x;
+    float* d_y;
+    float* d_yest; //d_ quer dizer variável do dispositivo, convenção
+
+    err[0] = cudaMalloc(&d_x, N*sizeof(float));
+    err[1] = cudaMalloc(&d_y, N*sizeof(float));
+    err[2] = cudaMalloc(&d_yest, N*sizeof(float));
+
+    if ((err[0] != cudaSuccess) || (err[1] != cudaSuccess) || (err[2] != cudaSuccess))
+    {
+        fprintf(stderr, "Failed to allocate on the Device\n");
+        exit(EXIT_FAILURE);
+    }
+
+    clock_gettime(CLOCK_REALTIME, &timeVect[2]);
+
+    printf(" ... Copying input data from the host memory to the CUDA device ...\n");
+    err[0] = cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+    err[1] = cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+    
+
+    if ((err[0] != cudaSuccess) || (err[1] != cudaSuccess))
+    {
+        fprintf(stderr, "Failed to copy data to the device!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    clock_gettime(CLOCK_REALTIME, &timeVect[3]);
+
+    printf(" ... CUDA kernel launch with %d blocks of %d threads ...\n", N/1024, 1024);
     // Máximo de 1024 threads concorrentes!
     for (size_t k = 0; k < N; k += 1024)
-        aproximateValuesGPU << <1, (N - k > 1024 ? 1024 : N - k) >> >(d_y + k, d_x + k, d_yest + k, smooth, N);
-
-    cudaMemcpy(yest, d_yest, N*sizeof(double), cudaMemcpyDeviceToHost);
-    printf("gpu done\n");
-#endif
-
-    double *yest2 = (double*)malloc(N * sizeof(double));
-    aproximateValuesCPU(y, x, yest2, smooth, N);
-    printf("cpu done\n");
-
-#ifndef CPUonly
-    for (size_t i = 0; i < N; i++)
     {
-        if (yest[i] != yest2[i])
-            printf(":( > yest2[%d] = %lf\n", i, yest2[i]);
-        printf("yest[%d] = %lf\n", i, yest[i]);
+        aproximateValuesGPU << <1, (N - k > 1024 ? 1024 : N - k) >> >(d_y + k, d_x + k, d_yest + k, smooth);
+        err[0] = cudaGetLastError();
+        if (err[0] != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to launch yest calculation kernel (error code %s)!\n", cudaGetErrorString(err[0]));
+            exit(EXIT_FAILURE);
+        }
     }
-#endif
+    clock_gettime(CLOCK_REALTIME, &timeVect[4]);
 
+    printf("Copy output data from the CUDA device to the host memory\n");
+    err[0] = cudaMemcpy(yest, d_yest, N*sizeof(float), cudaMemcpyDeviceToHost);
+    if (err[0] != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy yest from device to host (error code %s)!\n", cudaGetErrorString(err[0]));
+        exit(EXIT_FAILURE);
+    }
+    clock_gettime(CLOCK_REALTIME, &timeVect[5]);
+
+    err[0] = cudaFree(d_x);
+    err[1] = cudaFree(d_y);
+    err[2] = cudaFree(d_yest);
+
+    if ((err[0] != cudaSuccess) || (err[1] != cudaSuccess) || (err[2] != cudaSuccess))
+    {
+        fprintf(stderr, "Failed to free device vectors!\n");
+        exit(EXIT_FAILURE);
+    }
+    clock_gettime(CLOCK_REALTIME, &timeVect[6]);
+    
+    timeGPU[0] = timeDiff(timeVect[0],timeVect[1]);
+    timeGPU[1] = timeDiff(timeVect[1],timeVect[2]);
+    timeGPU[2] = timeDiff(timeVect[2],timeVect[3]);
+    timeGPU[3] = timeDiff(timeVect[3],timeVect[4]);
+    timeGPU[4] = timeDiff(timeVect[4],timeVect[5]);
+    timeGPU[5] = timeDiff(timeVect[5],timeVect[6]);
+    timeGPU[6] = timeDiff(timeVect[0],timeVect[6]);
+    printf("gpu done  ... execution took %.6f seconds (speedup=%.3f), corresponging to:\n",timeGPU[6],timeCPU/timeGPU[6]);
+    printf("          - first call to the device           -> %.6f seconds\n",timeGPU[0]);
+    printf("          - allocation of memory on the device -> %.6f seconds\n",timeGPU[1]);
+    printf("          - copying data from host to device   -> %.6f seconds\n",timeGPU[2]);
+    printf("          - kernel execution on the device     -> %.6f seconds\n",timeGPU[3]);
+    printf("          - copying data from device to host   -> %.6f seconds\n",timeGPU[4]);
+    printf("          - freeing data on the device         -> %.6f seconds\n",timeGPU[5]);
+    printf("----------------------------------------------------------------------------\n");
+
+    int errors = 0;
+    float avgerror=0;
+    for (size_t i = 0; i < N; i+=32)
+    {
+        if (yest[i] != yestCPU[i])
+        {
+            //printf("error found: yestCPU[%d] = %f : yestCPU[%d] = %f : error = %.24f\n", i, yestCPU[i], i, yest[i], yestCPU[i]-yest[i]);
+            errors++;
+            avgerror +=  abs(yestCPU[i]-yest[i]);
+        }
+        //    printf(":( > yestCPU[%d] = %f\n", i, yestCPU[i]);
+        //printf("yest[%d] = %f\n", i, yest[i]);
+    }
+
+    if(errors)
+        printf("\nTest Failed:\n\t %d errors found! (out of %d points, %d%%)\n\t average error: %f\n", errors, N, errors*100/N, avgerror/errors);    
+    else
+        printf("Test Passes\n");
+    
     free(x);
     free(y);
     free(yest);
-    free(yest2);
+    free(yestCPU);  
 
-#ifndef CPUonly
-    cudaFree(d_x);
-    cudaFree(d_y);
-    cudaFree(d_yest);
-#endif
+    // Reset the device and exit
+    err[0] = cudaDeviceReset();
 
+    if (err[0] != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(err[0]));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Done\n");
+    return 0;
 }
