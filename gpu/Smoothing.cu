303d9b6e8b07a55aeb2737580044c20f75593ede
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-05-23 20:57:18 +0100

    correcção para + de 1024 valores e organização de código

--- gpu/Smoothing.cu
+++ gpu/Smoothing.cu
@@ -1,81 +1,113 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda.h>
 
-#define CPUonly
+//#define CPUonly
 
 void generateData(double vectY[], const double vectX[], size_t vectXSize)
 {
-  for (size_t i = 0; i < vectXSize; i++)
-  {
-    vectY[i] = sin(0.02 * vectX[i]) + sin(0.001 * vectX[i]) + 0.1 * (rand() / (1.0 * RAND_MAX));
-  }
+    for (size_t i = 0; i < vectXSize; i++)
+        vectY[i] = sin(0.02 * vectX[i]) + sin(0.001 * vectX[i]) + 0.1 * (rand() / (1.0 * RAND_MAX));
 }
 
-#ifdef CPUonly
-void aproximateValues(double y[], double x[], double yest[], int smooth, size_t N)
+void aproximateValuesCPU(double y[], double x[], double yest[], int smooth, size_t N)
 {
-  for (size_t i = 0; i < N; i++)
-  {
-#else
-__global__ void aproximateValues(double y[], double x[], double yest[], int smooth, size_t N)
-{
-  size_t i = threadIdx.x;
-  if (i < N)
-  {
-#endif
-    double sumA = 0.0;
-    double sumB = 0.0;
-    double temp = 0.0;
-    for (size_t j = 0; j < N; j++)
+    for (size_t i = 0; i < N; i++)
     {
-      temp = exp((-1 * ((x[i] - x[j]) * (x[i] - x[j]))) / (2 * smooth * smooth));
-      sumA = sumA + temp * y[j];
-      sumB = sumB + temp;
+        double sumA = 0.0;
+        double sumB = 0.0;
+        double temp = 0.0;
+        for (size_t j = 0; j < N; j++)
+        {
+            temp = exp((-1 * ((x[i] - x[j]) * (x[i] - x[j]))) / (2 * smooth * smooth));
+            sumA = sumA + temp * y[j];
+            sumB = sumB + temp;
+        }
+        yest[i] = sumA / sumB;
     }
-    yest[i] = sumA/sumB;
-  }
+}
+
+__global__ void aproximateValuesGPU(double y[], double x[], double yest[], int smooth, size_t N)
+{
+    size_t i = threadIdx.x;
+    if (i < N)
+    {
+        double sumA = 0.0;
+        double sumB = 0.0;
+        double temp = 0.0;
+        for (size_t j = 0; j < N; j++)
+        {
+            temp = exp((-1 * ((x[i] - x[j]) * (x[i] - x[j]))) / (2 * smooth * smooth));
+            sumA = sumA + temp * y[j];
+            sumB = sumB + temp;
+        }
+        yest[i] = sumA / sumB;
+    }
 }
 
 
 int main()
 {
 
-  size_t N = 10000;  // numero de ponto no dataset
-  int smooth = 4; // parametro de suavidade
+    size_t N = 10000;  // numero de ponto no dataset
+    int smooth = 4; // parametro de suavidade
 
-  // criar o dataset de entrada
-  double *x = (double*)malloc(N * sizeof(double));
-  double *y = (double*)malloc(N * sizeof(double));
-  double *yest = (double*)malloc(N * sizeof(double));
+    // criar o dataset de entrada
+    double *x = (double*)malloc(N * sizeof(double));
+    double *y = (double*)malloc(N * sizeof(double));
+    double *yest = (double*)malloc(N * sizeof(double));
 
-  for (size_t i = 0; i < N; i++) {
-    x[i] = i / 10.0;
-  }
-  memset(yest, 0, N*sizeof(double));
+    for (size_t i = 0; i < N; i++)
+        x[i] = i / 10.0;
 
-  generateData(y, x, N);
+    generateData(y, x, N);
 
 #ifdef CPUonly  
-  aproximateValues(y, x, yest, smooth, N);
+    aproximateValuesCPU(y, x, yest, smooth, N);
+
 #else
-  double* d_x;
-  double* d_y;
-  double* d_yest; //d_ quer dizer variável do dispositivo, convenção
+    double* d_x;
+    double* d_y;
+    double* d_yest; //d_ quer dizer variável do dispositivo, convenção
 
-  cudaMalloc(&d_x, N*sizeof(double));
-  cudaMalloc(&d_y, N*sizeof(double));
-  cudaMalloc(&d_yest, N*sizeof(double));
+    cudaMalloc(&d_x, N*sizeof(double));
+    cudaMalloc(&d_y, N*sizeof(double));
+    cudaMalloc(&d_yest, N*sizeof(double));
 
-  cudaMemcpy(d_x, x, N*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_yest, yest, N*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, N*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, N*sizeof(double), cudaMemcpyHostToDevice);
 
-  aproximateValues << <1, N >> > (d_y, d_x, d_yest, smooth, N);
+    // Máximo de 1024 threads concorrentes!
+    for (size_t k = 0; k < N; k += 1024)
+        aproximateValuesGPU << <1, (N - k > 1024 ? 1024 : N - k) >> >(d_y + k, d_x + k, d_yest + k, smooth, N);
 
-  cudaMemcpy(yest, d_yest, N*sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(yest, d_yest, N*sizeof(double), cudaMemcpyDeviceToHost);
+    printf("gpu done\n");
 #endif
-  for (size_t i = 0; i < N; i++)
-  {
-    printf("yest[%d] = %lf\n", i, yest[i]);
-  }
+
+    double *yest2 = (double*)malloc(N * sizeof(double));
+    aproximateValuesCPU(y, x, yest2, smooth, N);
+    printf("cpu done\n");
+
+#ifndef CPUonly
+    for (size_t i = 0; i < N; i++)
+    {
+        if (yest[i] != yest2[i])
+            printf(":( > yest2[%d] = %lf\n", i, yest2[i]);
+        printf("yest[%d] = %lf\n", i, yest[i]);
+    }
+#endif
+
+    free(x);
+    free(y);
+    free(yest);
+    free(yest2);
+
+#ifndef CPUonly
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_yest);
+#endif
+
 }
